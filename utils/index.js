db51ff402de5f30d0539d4733e283a3c9deb15c1
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-22 15:56:17 -0400

    added questions and init fucntion

---
+++ utils/index.js
@@ -0,0 +1,65 @@
+// required packages
+
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+
+//Create questions that ask from the array. 1.Manager's name, Employee ID, email address, office number.
+//These are asked for in prompts.
+//Prompt selection between and intern engineer
+//Then I can enter the Engineer's name, ID, email and GitHub username
+//Select the intern option and the above is repeated
+// An HTML file is generated
+
+const questions = ([
+    {
+        type: 'input',
+        name: 'managername',
+        message: 'What is the name of your manager?'
+    },
+
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'What is your employee ID?'
+    },
+
+    {
+        type: 'input',
+        name: 'emailaddress',
+        message: 'What is your email address?'
+    },
+
+    {
+        type: 'input',
+        name: 'officenumber',
+        message: 'what is your office number?'
+    },
+
+    {
+        type: 'list',
+        name: 'Stafftype',
+        message: 'Are you an engineer or a intern',
+        choices: ['Intern', 'Engineer']
+    }
+]);
+
+const generateHTML = ({managername, employeeID, emailaddress, officenumber, stafftype
+
+}) =>
+
+//Skeleton of document
+
+//initialize application
+function init() {
+    inquirer.createPromptModule(questions)
+    .then ((inquirerResponse) =>{
+        console.log(inquirerResponse)
+
+        console.log('Writing HTML');
+        fs.writeFileSync("./output/index.html",
+       generateHTML(inquirerResponse) )
+    })
+}
+
+init()
